3331956a7ed8599db356797034c89330f3176a47
GIRDHARI KUMAR <107917375+girdharik@users.noreply.github.com>
2023-08-12 15:11:47 +0000

    ex

--- exercises/projects/store_dag.py
+++ exercises/projects/store_dag.py
@@ -1,6 +1,11 @@
 from datetime import datetime, timedelta
 from airflow import DAG
 from airflow.operators.bash import BashOperator
+from airflow.operators.python import PythonOperator
+from datacleaner import data_cleaner
+
+from airflow.providers.mysql.operators.mysql import MySqlOperator
+
 
 default_args = {
     'owner': 'Airflow',
@@ -12,9 +17,15 @@ default_args = {
 dag = DAG('store_dag', default_args=default_args, schedule_interval='@daily', catchup=False)
 
 t1 = BashOperator(
-    task_id='check_files_exits',
+    task_id='check_files_exists',
     bash_command='shasum gs://airflow_learning_projects/raw_store_transactions.csv',
     retries=2,
     retry_delay=timedelta(seconds=15),
+    dag=dag
+)
+
+t2 = PythonOperator(
+    task_id='clean_raw_csv',
+    python_callable=data_cleaner,
     dag=dag
 )
--- learnings/airflow101.py
+++ learnings/airflow101.py
@@ -1,26 +1,26 @@
-#import the required libraries
-from airflow import DAG
+# import the required libraries
 from datetime import datetime, timedelta
-from airflow.operators import BashOperator
+from airflow import DAG
+from airflow.operators.bash import BashOperator
 
 
-#defining the default arguments dictionary
+# defining the default arguments dictionary
 args = {
-    'owner': 'airflow'
-    'start_date': 'datetime(2023,08,10)'
-    'retries': 1
-    "retry_delay": timedelta(seconds=10),
+    'owner': 'airflow',
+    'start_date': datetime(2023, 8, 10),
+    'retries': 1,
+    "retry_delay": timedelta(seconds=10)
 }
 
 dag = DAG('sample', default_args=args)
 
-#task1 is to create a directory 'test_dir' inside the dags folder
+# task1 is to create a directory 'test_dir' inside the dags folder
 task1 = BashOperator(task_id='create_directory',
                      bash_command='mkdir ~/dags/test_dir', dag=dag)
 
-#task2 is to get the 'shasum' of 'test_dir' directory
+# task2 is to get the 'shasum' of 'test_dir' directory
 task2 = BashOperator(task_id='get_shasum',
                      bash_command='shasum ~/dags/test_dir', dag=dag)
 
-#defining relationship
+# defining relationship
 task1 >> task2
